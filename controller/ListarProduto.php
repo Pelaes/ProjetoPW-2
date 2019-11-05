7829a6bec792a0a967dab011bd25d7fa2218a178
Laaiiix <Laisalves055@gmail.com>
2019-11-04 22:08:47 -0300

    Teste de segurança

---
+++ controller/ListarProduto.php
@@ -0,0 +1,17 @@
+<?php
+
+    $pagina="listarproduto.php"; //aqui colocariamos o nome da pagina.
+    if(basename($_SERVER["PHP_SELF"]) == $pagina){
+    die("<script>alert('Sem permição de acesso !')</script>\n<script>window.location=('../index.php')</script>");
+    // echo "caraio";
+    // die();
+    }
+
+    function ValidarBusca()
+    {
+        //include "../model/produtoModel.php";
+        //buscarProdutos();
+        echo "teste";
+    }
+
+?>
